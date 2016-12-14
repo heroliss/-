20d2499e8883afcaabf44355ec02e16d2e6d622f
heroliss <ss1992@126.com>
2016-12-14 11:35:00 +0800

    0.11 稍微扩展了基础框架

--- GA_config.py
+++ GA_config.py
@@ -31,10 +31,13 @@ class GAConfig:
         return self.pop_size
 
     @pop_size.setter
-    def pop_size(self,value):
+    def pop_size(self, value):
         # 设置种群规模
         if isinstance(value, int) and value > 0:
             self.pop_size = value
         else:
             raise ValueError('种群规模必须为正整数')
 
+    mutate = None  # 变异函数
+    cross = None  # 交叉函数
+    select =None  # 选择函数
